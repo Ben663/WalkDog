b3bafda0fba8fef42f865b4c5dae9c10310d0142
Ben663 <87435130+Ben663@users.noreply.github.com>
2023-01-02 18:47:52 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ import './db/mongoose.connect.js'
 import cors from 'cors'
 import roomRouter from "./routes/roomRouter.js";
 import userRouter from './routes/userRouter.js'
-// import * as url from 'url'
-// import path from 'path'
-// const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+import * as url from 'url'
+import path from 'path'
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 
 
 const port = process.env.PORT || 5000;
@@ -19,15 +19,15 @@ app.use((_, res, next) => {
 });
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
-// const publicPath = path.join(__dirname, 'build');
-// app.use(express.static(publicPath));
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
 app.use('/user', userRouter);
 app.use('/room', roomRouter);
 
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, 'build', index.html));
-// });
-app.get('/', (_, res) => res.json({ message: 'Welcome To Our API' }));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'build', index.html));
+});
+// app.get('/', (_, res) => res.json({ message: 'Welcome To Our API' }));
 app.use((_, res) => res.status(404).json({ success: false, message: 'Not Found' }));
 
 const startServer = async () => {
