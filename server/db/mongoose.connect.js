ec94326612ddefbbaf8d13dea26c687062fc05d0
Ben663 <87435130+Ben663@users.noreply.github.com>
2023-01-04 00:17:53 +0200

    changed .env props

--- server/db/mongoose.connect.js
+++ server/db/mongoose.connect.js
@@ -1,18 +1,22 @@
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv'
+/** @format */
+
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const URL = process.env.MONGO_URL;
-mongoose.set('strictQuery', false)
+const userName = process.env.MONGO_USER;
+const userPass = process.env.MONGO_PASS;
+
+const URL = `mongodb+srv://${userName}:${userPass}@walkdog.3zgw6et.mongodb.net/WalkDog?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
 try {
 	mongoose.connect(URL, (err, mongoDbInstance) => {
 		if (err) throw Error('connectin faild : ' + err);
 		const { host, port, name } = mongoDbInstance;
-		console.log({ host, port, name },`Good Job MongoDB Is Connected`);
-		
+		console.log({ host, port, name }, `Good Job MongoDB Is Connected`);
 	});
-	} catch (error) {
-		console.log('Houston We Have A Problem');
-	}
-	// 
+} catch (error) {
+	console.log('Houston We Have A Problem');
+}
+//
